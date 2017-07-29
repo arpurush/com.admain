5e590be240acb110ccb53f85076bc3e7f0afc6c8
Arun <engrarun.p@gmail.com>
2017-07-29 23:47:20 +0000

    moved the agent code to a different repo

--- admain-agent/agent/agentRoutes.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app){
-    var systemInfo = require('./agentControllers/systemInfo');
-    app.get('/heartbeat', systemInfo.getCurrentTime);
-    app.get('/storage', systemInfo.getStorageInfo);
-}
